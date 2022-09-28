c915c85c1cfb0c855984619505b5fce1af884c11
MichaelaTarryn <mward7006@gmail.com>
2022-09-28 12:55:41 +0200

    second half of the day

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,9 +1,40 @@
 <template>
-
+<nav>
+    <v-navigation-drawer v-model="drawer" dark app mini-variant mini-variant-width="100" class="teal">
+ <v-list>
+    <v-list-item class="mb-12">
+        <v-list-item-content>
+            <v-icon class="mb-2" large>  fas fa-hand-holding-medical </v-icon>
+            <v-list-item-subtitle align="center">medi</v-list-item-subtitle>
+        </v-list-item-content>
+    </v-list-item>
+ </v-list>
+ <v-list flat>
+    <v-list-item router to="/">
+        <v-list-item-content>
+            <v-icon class="mb-2">
+                fas fa-home
+            </v-icon>
+        </v-list-item-content>
+    </v-list-item>
+    <v-list-item router to="/">
+        <v-badge color="pink" dot overlap>
+            <v-list-item-content>
+                <v-icon class="mb-2">
+                    fas fa-user
+                </v-icon>
+            </v-list-item-content>
+        </v-badge>
+    </v-list-item>
+ </v-list>
+    </v-navigation-drawer>
+</nav>
 </template>
 <script>
     export default{
-        data:()=
+        data:()=>({
+            drawer:true
+        })
     }
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-container>
-
+hellloo
   </div>
 </template>
 
